9d7ecb5ff0e672bf7d27076fd42ff2bd66cdb59f
Shion <shion@shion1118.com>
2015-03-31 00:44:02 +0900

    First Commit

---
+++ src/main/java/com/shion1118/friends/FriendEvent.java
@@ -0,0 +1,7 @@
+package com.shion1118.friends;
+
+import org.bukkit.event.Listener;
+
+public class FriendEvent implements Listener {
+
+}
---
+++ src/main/java/com/shion1118/friends/Friends.java
@@ -0,0 +1,287 @@
+package com.shion1118.friends;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.shion1118.friends.FriendEvent;
+
+public class Friends extends JavaPlugin {
+
+	public static File friendFile;
+	public static FileConfiguration frienddata;
+
+	HashMap<Player, ArrayList<Player>> request = new HashMap<Player, ArrayList<Player>>();
+	HashMap<Player, Integer> requesttime = new HashMap<Player, Integer>();
+
+    @Override
+    public void onEnable() {
+
+    	registerEvents();
+
+        try {
+			setupConfig();
+		} catch (IOException | InvalidConfigurationException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+    }
+
+    @Override
+    public void onDisable() {
+    	getServer().getConsoleSender().sendMessage("[Friends]" + ChatColor.DARK_AQUA + " Disable");
+    }
+
+    private void registerEvents() {
+    	PluginManager pm = Bukkit.getPluginManager();
+    	pm.registerEvents(new FriendEvent(), this);
+    }
+
+    private void setupConfig() throws IOException, InvalidConfigurationException{
+    	//FriendData Config
+    	friendFile = new File(getDataFolder(), "friend.yml");
+    	if(!friendFile.exists()){
+    		friendFile.getParentFile().mkdirs();         // creates the /plugins/<pluginName>/ directory if not found
+            copy(getResource("friend.yml"),friendFile);
+    	}
+    	frienddata = new YamlConfiguration();
+    	frienddata.load(friendFile);
+    }
+
+    private void copy(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (sender instanceof Player) {
+    		Player p = (Player) sender;
+    		if(cmd.getName().equalsIgnoreCase("friend")){
+    			if(args.length == 2){
+    				if(args[0].equalsIgnoreCase("request")){
+    					requestFriend(p,args[1]);
+    					return true;
+    				}
+    				if(args[0].equalsIgnoreCase("accept")){
+    					acceptFriend(p,args[1]);
+    					return true;
+    				}
+    				if(args[0].equalsIgnoreCase("info")){
+    					infoFriend(p,args[1]);
+    					return true;
+    				}
+    				if(args[0].equalsIgnoreCase("remove")){
+    					removeFriend(p,args[1]);
+    					return true;
+    				}
+    			}
+    			if(args.length == 1){
+    				if(args[0].equalsIgnoreCase("list")){
+        				listFriend(p);
+        				return true;
+        			}
+    			}
+
+    			p.sendMessage("§6Friends Command List");
+    			p.sendMessage("§6/friend request <name>");
+    			p.sendMessage("§6/friend accept <name>");
+    			p.sendMessage("§6/friend info <name>");
+    			p.sendMessage("§6/friend remove <name>");
+    			p.sendMessage("§6/friend list");
+    			p.sendMessage("§6This plugin made by Shion.");
+
+    		}
+    		return true;
+    	} else {
+    		sender.sendMessage("This Command for Player!");
+    		return false;
+    	}
+    }
+
+    public void requestFriend(Player p, String name){
+    	Player friend = Bukkit.getServer().getPlayer(name);
+    	ArrayList<Player> list = new ArrayList<Player>();
+    	if(friend == null){
+    		p.sendMessage(name + " §adoes not online");
+    		return;
+    	}
+    	if(frienddata.contains(p.getName() + "." + name)){
+    		p.sendMessage(name + "§e is already your Friend.");
+    		return;
+    	}
+
+    	friend.sendMessage("§e" + p.getName() +" send Friend request to you.");
+    	friend.sendMessage("§eif you accept, please type §f/friend accept " + p.getName());
+
+    	if(request.containsKey(friend)){
+    		list = request.get(friend);
+    	}
+    	list.add(friend);
+    	request.put(p, list);
+    	requestTime(p,name);
+
+    	return;
+    }
+
+    public void acceptFriend(Player p, String name){
+    	Player friend = Bukkit.getServer().getPlayer(name);
+    	ArrayList<Player> list = new ArrayList<Player>();
+    	if(!checkOnline(name)){
+    		p.sendMessage(name + " §adoes not online");
+    		return;
+    	}
+    	if(!request.containsKey(friend)){
+    		p.sendMessage(name + " §adoes not send Friend request to you.");
+    		return;
+    	} else if(!request.get(friend).contains(p)){
+    		p.sendMessage(name + " §adoes not send Friend request to you.");
+    		return;
+    	}
+
+    	Bukkit.getServer().getScheduler().cancelTask(requesttime.get(friend));
+    	requesttime.remove(friend);
+
+    	list = request.get(friend);
+    	list.remove(p);
+    	frienddata.set(name+ "." + p.getName(), "");
+    	frienddata.set(p.getName() + "." + name, "");
+
+    	p.sendMessage("§eAdd Friend §f" + name + "§e.");
+    	friend.sendMessage(p.getName() + " §eAccept your Friend request.");
+
+    	try {
+			frienddata.save(friendFile);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+    	return;
+    }
+
+    public void requestTime(final Player p, final String name){
+    	int requestID = Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
+    		public void run(){
+    			request.remove(p);
+    			p.sendMessage(name + " §adose not accept your Friend request.");
+    		}
+    	}, 1200L);
+    	requesttime.put(p, requestID);
+    }
+
+    public void infoFriend(Player p,String name){
+    	if(!frienddata.contains(p.getName() + "." + name)){
+    		p.sendMessage(name + " §ais not your Friend.");
+    		return;
+    	}
+
+    	String online;
+    	if(checkOnline(name)){
+    		online = "§bOnline";
+    	}else{
+    		online = "§cOffline";
+    	}
+
+    	p.sendMessage("§e-----Friend Info-----");
+    	p.sendMessage("§eID:§f" + name + " §eStatus:" + online);
+    	if(checkOnline(name)){
+    		p.sendMessage("§eLocationX: §f" + String.format("%.1f", checkLocationX(name)));
+    		p.sendMessage("§eLocationY: §f" + String.format("%.1f", checkLocationY(name)));
+    		p.sendMessage("§eLocationZ: §f" + String.format("%.1f", checkLocationZ(name)));
+    	}
+
+    	return;
+    }
+
+    public void removeFriend(Player p,String name){
+    	if(!frienddata.contains(p.getName() + "." + name)){
+    		p.sendMessage(name + " §ais not your Friend.");
+    		return;
+    	}
+
+    	frienddata.set(p.getName() + "." + name, null);
+    	frienddata.set(name + "." + p.getName(), null);
+    	p.sendMessage("§eRemove your Friend §f" + name);
+
+    	try {
+			frienddata.save(friendFile);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+    	return;
+    }
+
+    public void listFriend(Player p){
+    	if(frienddata.isConfigurationSection(p.getName())){
+    		for (String s :frienddata.getConfigurationSection(p.getName()).getKeys(false)){
+    			p.sendMessage(s);
+    		}
+    	}
+    	return;
+    }
+
+    public boolean checkOnline(String name){
+    	Player p = Bukkit.getServer().getPlayer(name);
+    	if(p == null){
+    		return false;
+    	}else{
+    		return true;
+    	}
+
+    }
+
+    public double checkLocationX(String name){
+    	Player p = Bukkit.getServer().getPlayer(name);
+    	if(p == null){
+    		return 0;
+    	}
+
+    	return p.getLocation().getX();
+    }
+
+    public double checkLocationY(String name){
+    	Player p = Bukkit.getServer().getPlayer(name);
+    	if(p == null){
+    		return 0;
+    	}
+
+    	return p.getLocation().getY();
+    }
+
+    public double checkLocationZ(String name){
+    	Player p = Bukkit.getServer().getPlayer(name);
+    	if(p == null){
+    		return 0;
+    	}
+
+    	return p.getLocation().getZ();
+    }
+
+}
